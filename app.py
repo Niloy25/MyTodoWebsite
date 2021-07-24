80a067d60ae51ed641426949062e2e5f37dd73f1
Niloy Mondal <niloykolkata59@gmail.com>
2021-07-24 20:26:22 +0530

    Final Ultimate Changes

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ class Todo(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     desc = db.Column(db.String(500), nullable=False)
-    date = db.Column(db.DateTime, default=datetime.utcnow)
+    date = db.Column(db.DateTime, default=datetime.now(tz=None))
 
     def __repr__(self) -> str:
         return f"{self.sno} - {self.title}"
